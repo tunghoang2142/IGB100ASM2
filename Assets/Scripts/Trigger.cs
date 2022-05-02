67109c260ae2043175a949fb8acf12c795fe2284
Tung <93835906+tunghoang2142@users.noreply.github.com>
2022-05-02 16:00:17 +1000

    Add UIManager

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public string dialog;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(UIManager.Instance.DisplayDialog(dialog));
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public TMP_Text dialog;
+
+    static UIManager _instance;
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public static UIManager Instance { get { return _instance; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator DisplayDialog(string text)
+    {
+        dialog.text = text;
+        dialog.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        dialog.gameObject.SetActive(false);
+    }
+}
